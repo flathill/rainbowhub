69fe99aafd0d5aad88c4e2dbbdf42afbc0686436
flathill <seiichirou.hiraoka@gmail.com>
2022-03-26 18:25:42 +0900

    add debug, verbose

--- rainbowcrack.py
+++ rainbowcrack.py
@@ -8,10 +8,13 @@ try:
     parser.add_argument("hash_string", help="hash to crack")
     parser.add_argument("rainbow_table_file", help="name of file containing a valid rainbow table"
     " (generated from rainbowgen.py)")
+    parser.add_argument('-d', '--debug', action='store_true')
+    parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
 
     rt = RainbowTable.load_from_file(args.rainbow_table_file)
-    psw = rt.lookup(args.hash_string)
+    psw = rt.lookup(args.hash_string, args.verbose, args.debug)
+
     if(psw is not None):
         print("Candidate found: " + psw)
     else:
