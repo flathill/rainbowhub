405a5655b0830b0a22ca1ce07e6564f790868a04
flathill <seiichirou.hiraoka@gmail.com>
2022-03-27 10:52:06 +0900

    Modified so that rainbow_table_file can be specified.

--- rainbowcrack.py
+++ rainbowcrack.py
@@ -6,14 +6,13 @@ from rainbowtable import RainbowTable
 try:
     parser = argparse.ArgumentParser()
     parser.add_argument("hash_string", help="hash to crack")
-    parser.add_argument("rainbow_table_file", help="name of file containing a valid rainbow table"
-    " (generated from rainbowgen.py)")
+    parser.add_argument('-r', '--rainbow_table_file', default = 'result.rt', help="name of rainbow table file")
     parser.add_argument('-d', '--debug', action='store_true')
     parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
 
     rt = RainbowTable.load_from_file(args.rainbow_table_file)
-    psw = rt.lookup(args.hash_string, args.verbose, args.debug)
+    psw = rt.lookup(args.hash_string, args.debug)
 
     if(psw is not None):
         print("Candidate found: " + psw)
