419042f98c991645e6ad0c60047fd79c726c60a5
flathill <seiichirou.hiraoka@gmail.com>
2022-03-28 23:15:20 +0900

    add exception traceback

--- rainbowgen.py
+++ rainbowgen.py
@@ -1,16 +1,18 @@
+#!/usr/bin/env python3
 import sys
 import os
 import argparse
+import traceback
 from rainbowtable import RainbowTable
 
 try:
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--algorithm', default = 'sha256',      help="md5 or sha1 or sha224 or sha256 or sha384 or sha512")
-    parser.add_argument('-c', '--charset', default = 'dictionary',    help="charset must be included in config.ini")
-    parser.add_argument('-m', '--min_length', default = 6,            help="minimum length of passwords",type=int)
-    parser.add_argument('-M', '--max_length', default = 8,            help="maximum length of passwords",type=int)
-    parser.add_argument('-l', '--chain_length', default = 2,          help="length of each chain",type=int)
-    parser.add_argument('-n', '--number_of_chains', default = 2,      help="number of chains generated",type=int)
+    parser.add_argument('-a', '--algorithm', default = 'sha256', help="md5 or sha1 or sha224 or sha256 or sha384 or sha512")
+    parser.add_argument('-c', '--charset', default = 'dict',     help="charset must be included in config.ini")
+    parser.add_argument('-m', '--min_length', default = 6,       help="minimum length of passwords",type=int)
+    parser.add_argument('-M', '--max_length', default = 8,       help="maximum length of passwords",type=int)
+    parser.add_argument('-l', '--chain_length', default = 2,     help="length of each chain",type=int)
+    parser.add_argument('-n', '--number_of_chains', default = 2, help="number of chains generated",type=int)
     parser.add_argument('-r', '--rainbow_table_file', default = 'result.rt', help="name of rainbow table file")
     parser.add_argument('-d', '--debug', action='store_true')
     parser.add_argument('-v', '--verbose', action='store_true')
@@ -21,4 +23,11 @@ try:
     rt.save_to_file(args.rainbow_table_file)
 
 except Exception as e:
-    print("ERROR: " + str(e))
+    exception_type, exception_object, exception_traceback = sys.exc_info()
+    filename = exception_traceback.tb_frame.f_code.co_filename
+    line_number = exception_traceback.tb_lineno
+
+    print("Exception type: ", exception_type)
+    print("File name: ", filename)
+    print("Line number: ", line_number)
+    print(traceback.format_exc())
